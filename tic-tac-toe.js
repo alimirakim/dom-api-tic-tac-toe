69923f4d119f3b912aa08dc43cc9d563271228a9
bacbartlett <brandonacb@gmail.com>
2020-08-06 17:35:00 -0400

    Basic functionality and adding classes

---
+++ tic-tac-toe.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", function(){
+    const board = document.getElementById("tic-tac-toe-board");
+    const x = 
+
+
+    board.addEventListener("click", function(event){
+        if(){
+            event.target.classList.add("x-move")
+        }
+    })
+})
