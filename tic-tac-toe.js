ff968c189174d747795e7bcf4992ddf1a6ea074e
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2020-08-06 17:52:16 -0400

    Checks x/o turns, draws an x/o only if empty spot.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,22 @@
 window.addEventListener("DOMContentLoaded", function(){
     const board = document.getElementById("tic-tac-toe-board");
-    const x = 
 
-
+// Check x/o turn, draw x/o if box is empty
+    let xTurn = true
     board.addEventListener("click", function(event){
-        if(){
+        if (xTurn && ![...event.target.classList].includes("x-move") && ![...event.target.classList].includes("o-move")) {
             event.target.classList.add("x-move")
+            xTurn = false;
+        } else if (!xTurn && ![...event.target.classList].includes("x-move") && ![...event.target.classList].includes("o-move")) {
+            event.target.classList.add("o-move");
+            xTurn = true;
         }
     })
+
+function checkWinner(id) {
+// Check the id of the last move, compare and check if there's a winner, return x/o/null
+// 2-3 wins - check horiz/vert/opt-diag
+
+}
+
 })
